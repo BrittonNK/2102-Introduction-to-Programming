ec9ed7b954768f57ad33d59c46d58bf5c772c0f3
Britton Kinuthia <153991239+BrittonNK@users.noreply.github.com>
2024-12-09 23:34:20 +0300

    Division Game.c
    A program is required for a computer game. The user keys in the number of rounds he wishes toplay.For each round the user enters his lucky number. The program takes the number and divides it withasecret number. If the remainder of the division is zero, it is considered a draw for the roundandthetotalscore is incriminated by 1. Otherwise if it is any other even number, it is considered a winfor theroundand the total score is incremented by 3. However if it is an odd number, it is considered a loss fortheround and the total score is decremented by 3. This is done until he completes his rounds. Hewinsifthetotal score at the end is a positive number.

---
+++ Division Game.c
@@ -0,0 +1,56 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+int main(){
+ int rounds,counter,lucky_number,secret_num,rem,total,score;
+ float div_ans;
+ counter=1;
+ rounds=0;
+ lucky_number=0;
+ rem=0;
+ score=0;
+ secret_num=0;
+ srand(time(0));
+ printf("How many rounds do you want to play?\n");
+ scanf("%d",&rounds);
+ while (counter<=rounds){
+ printf("Input your lucky number for round %d:\n",counter);
+ scanf("%d",&lucky_number);
+ do{
+ secret_num = rand()%100;
+ }while (secret_num == 0);
+ div_ans= (float)lucky_number/(float)secret_num;
+ rem = lucky_number%secret_num;
+ printf("Your lucky number is: %d and the computer generated secret number
+is: %d \n",lucky_number,secret_num);
+ if (rem == 0){
+ score+= 1;
+ printf("The division answer of the two is: %.2f \n",div_ans);
+ printf("The remainder of the division is: %d\n",rem);
+ printf("Round No.%d is a draw!! \n",counter);
+ }
+ else if(rem%2 == 0){
+ score+=3;
+ printf("The division answer of the two is: %.2f \n",div_ans);
+ printf("The remainder of the division is: %d\n",rem);
+ printf("Round No.%d is a Win!! \n",counter);
+ }
+ else{
+ score-=3;
+ printf("The division answer of the two is: %.2f \n",div_ans);
+ printf("The remainder of the division is: %d\n",rem);
+ printf("Round No.%d is a Loss!! \n",counter);
+ }
+
+printf("----------------------------------------------------------------------\n");
+ counter++;
+ }
+ total=rounds*3;
+ printf("Your score is %d/%d\n",score,total);
+ if (score>0){
+ printf("YOU WON THE GAME!!\n");
+ }
+ else{
+ printf("YOU LOST THE GAME!!\n");
+ }
+}
