3aa18615ee440bcfea469a96e9ee95ccc714d69d
Britton Kinuthia <kinuthiabritton@gmail.com>
2024-12-15 22:42:31 +0300

    Update to allow character input and proper instructions to the user

--- Assignment2/StoneKnifePaperGame/game.c
+++ Assignment2/StoneKnifePaperGame/game.c
@@ -1,42 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 int main() {
-    int computerchoice, userchoice;
+    int computer_choice, user_choice_mapped;
+    char user_choice;
 
 
     srand(time(NULL));
 
-    computerchoice = rand() % 3;
-
-
+    // Welcome & Game rules
     printf("Welcome to the Stone-Knife-Paper game!\n");
-    printf("Enter your choice: 0 for Stone, 1 for Knife, 2 for Paper: ");
-    scanf("%d", &userchoice);
+    printf("-------------------------------------------------------------- \n");
+    printf("Game Rules are as follows: \n");
+    printf("1. Stone blunts Knife. \n");
+    printf("2. Knife cuts Paper. \n");
+    printf("3. Paper wraps Stone. \n");
+    printf("Input 'S' for Stone, 'K' for Knife and 'P' for Paper. \n");
+    printf("-------------------------------------------------------------- \n");
 
+    //user input & Validation
+    do {
+        printf("Input your choice: \n");
+        scanf(" %c", &user_choice);
+        user_choice = toupper(user_choice);
+    }while(user_choice != 'S' && user_choice != 'K' && user_choice != 'P');
+
+    // Computer
+    computer_choice = rand() % 3;
+
+    //Map user input
+    if(user_choice == 'S') {
+        user_choice_mapped = 0;
+    }
+    else if(user_choice == 'K') {
+        user_choice_mapped = 1;
+    }
+    else if(user_choice == 'P') {
+        user_choice_mapped = 2;
+    }
+
+    printf("The Computer chose: ");
+    switch (computer_choice) {
+        case 0: printf("Stone\n"); break;
+        case 1: printf("Knife\n"); break;
+        case 2: printf("Paper\n"); break;
+    }
 
     printf("You chose: ");
-    switch (userchoice) {
+    switch (user_choice_mapped) {
         case 0: printf("Stone\n"); break;
         case 1: printf("Knife\n"); break;
         case 2: printf("Paper\n"); break;
 
     }
 
-    printf("Computer chose: ");
-    switch (computerchoice) {
-        case 0: printf("Stone\n"); break;
-        case 1: printf("Knife\n"); break;
-        case 2: printf("Paper\n"); break;
-    }
 
-
-    if (userchoice == computerchoice) {
+    if (user_choice_mapped == computer_choice) {
         printf("It's a draw!\n");
-    } else if ((userchoice == 0 && computerchoice == 1) ||
-               (userchoice == 1 && computerchoice == 2) ||
-               (userchoice == 2 && computerchoice == 0))
+    } else if ((user_choice_mapped == 0 && computer_choice == 1) ||
+               (user_choice_mapped == 1 && computer_choice == 2) ||
+               (user_choice_mapped == 2 && computer_choice == 0))
     {
         printf("You win!\n");
     } else
